f7fc436e74042345632772b8556ddc86c20b7073
Jake Weinstein <jake@aospa.co>
2022-08-09 07:29:10 +0900

    Spacewar: rootdir: Manually place msm_drm and msm_kgsl_3d0 IRQs
    After unbalancing, move these IRQs to separate cores
    to improve latency.
    
    Change-Id: Ib1c22f9c8d8c00cf74351099e88fbb5ae70e9114
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

--- device.mk
+++ device.mk
@@ -274,6 +274,7 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.kernel.post_boot-yupik.sh \
     init.kernel.post_boot.sh \
+    init.Spacewar.perf.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \

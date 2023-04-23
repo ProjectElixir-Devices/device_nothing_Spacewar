e11ad7c4b5d0fca93423ffce7ad9986155b48ab3
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-04-23 16:59:27 +0000

    Spacewar: Build servicetracker@1.2 vendor variant
    *F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.servicetracker@1.2-service": library "vendor.qti.hardware.servicetracker@1.2.so" not found: needed by main executable
    
    Signed-off-by: SageOfD6Path <mail2anirban95@gmail.com>
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

--- device.mk
+++ device.mk
@@ -459,6 +459,14 @@ PRODUCT_COPY_FILES += \
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 31
 
+# Service Tracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2-service \
+    vendor.qti.hardware.servicetracker@1.2-impl \
+    vendor.qti.hardware.servicetracker@1.0.vendor \
+    vendor.qti.hardware.servicetracker@1.1.vendor \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
 # SoC
 PROCESSOR_MODEL := SM7325
 

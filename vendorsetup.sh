26b63bd63cfb8a5e84a596d552e6e136414b76f0
NotHarshhaa <reddyharshhaa12@gmail.com>
2023-11-30 16:56:00 +0530

    [TEMP] Spacewar: vendorsetup: Track the updated repos for Jenkins Build
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,14 @@
 # Vendor Tree
-git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_nothing_Spacewar.git -b tiramisu vendor/nothing/Spacewar
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_nothing_Spacewar.git -b udc vendor/nothing/Spacewar
 
 # Kernel Tree
-git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_nothing_sm7325.git -b tiramisu kernel/nothing/sm7325
+git clone --depth=1 https://github.com/PixysOS-Devices/kernel_nothing_sm7325.git -b fourteen kernel/nothing/sm7325
 
 # Nothing Camera
-git clone --depth=1 https://gitlab.com/NotHarshhaa/vendor_nothing_camera.git -b xt vendor/nothing/camera
+git clone --depth=1 https://gitlab.com/PixysOS/vendor_nothing_camera.git -b thirteen vendor/nothing/camera
 
 # Elixir Glyph
 git clone --depth=1 https://github.com/ProjectElixir-Devices/packages_apps_ElixirGlyph.git -b tiramisu packages/apps/ElixirGlyph
+
+# Display Hal
+git clone --depth=1 https://github.com/NotHarshhaa/hardware_qcom-caf_sm8350_display.git -b fourteen hardware/qcom-caf/sm8350/display

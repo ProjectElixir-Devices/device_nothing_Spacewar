1b9e6416d1e059c33afe1d6bc752dd8fc5fc46ae
NotHarshhaa <reddyharshhaa12@gmail.com>
2024-02-21 14:24:40 +0530

    Spacewar: Add vendorsetup.sh & remove.txt for Jenkins Req..
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# Vendor Tree
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_nothing_Spacewar.git -b udc vendor/nothing/Spacewar
+
+# Kernel Tree
+git clone --depth=1 https://github.com/PixysOS-Devices/kernel_nothing_sm7325.git -b fourteen kernel/nothing/sm7325
+
+# Nothing Camera
+git clone --depth=1 https://gitlab.com/PixysOS/vendor_nothing_camera.git -b fourteen vendor/nothing/camera
